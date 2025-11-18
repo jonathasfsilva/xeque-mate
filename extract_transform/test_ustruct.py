f59d139ca88a463a92eff60892fd2bff03094691
lucas <lucass235@gmail.com>
2025-11-18 14:33:38 -0300

    update

--- extract_transform/test_ustruct.py
+++ extract_transform/test_ustruct.py
@@ -1,9 +1,9 @@
 from pathlib import Path
-from extract_transform.src.extract_transform import process_directory
+from extract_transform import process_directory
 
 
 SCRIPT_DIR = Path(__file__).resolve().parent
-PROJECT_ROOT = SCRIPT_DIR
+PROJECT_ROOT = SCRIPT_DIR.parent
 
 NAME_FILE = "NIST.SP.800-61r3.pdf"
 FILE_TO_PROCESS = PROJECT_ROOT / "coletor_data" / "data" / "external" / "acao_recomendada" / NAME_FILE
